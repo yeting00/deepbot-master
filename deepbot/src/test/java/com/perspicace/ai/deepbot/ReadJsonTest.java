cd25391c833e20dc097be3792c4b6ddaf4bbd821
yeting <263531773@qq.com>
2018-08-18 20:36:03 +0800

    测试类代码恢复

--- deepbot/src/test/java/com/perspicace/ai/deepbot/ReadJsonTest.java
+++ deepbot/src/test/java/com/perspicace/ai/deepbot/ReadJsonTest.java
@@ -1,25 +1,27 @@
-//package deepbot;
-//
-//import ch.qos.logback.core.util.FileUtil;
-//import lombok.extern.slf4j.Slf4j;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-///**
-// * @program: deepbot
-// * @description:
-// * @author: Destiny
-// * @create: 2018-07-11 20:16
-// **/
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//@Slf4j
-//public class ReadJsonTest {
-//    @Test
-//    public void read(){
-//        FileUtil.getJson("device.json");
-//    }
-//
-//}
+package com.perspicace.ai.deepbot;
+
+import com.perspicace.ai.deepbot.utils.FileUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+/**
+ * @program: deepbot
+ * @description:
+ * @author: Destiny
+ * @create: 2018-07-11 20:16
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class ReadJsonTest {
+    @Test
+    public void read(){
+        FileUtil.getJson("device.json");
+    }
+
+}
